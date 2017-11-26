e15d8370eb2bb116eb3c2ee6c2f4821db6079f6f
John Letey <30328854+JohnLetey@users.noreply.github.com>
2017-11-26 12:12:50 -0700

    Uploaded graphExample.py

---
+++ graphExample.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 11 15:29:04 2017
+
+@author: rhoenigman
+"""
+
+import networkx as nx
+import matplotlib.pylab as plt
+
+#create a directed graph
+G = nx.DiGraph()
+
+#adding an edge also adds the node
+G.add_edge('Spider', 'A', weight=1.0)
+G.add_edge('Spider', 'H', weight=1.0)
+G.add_edge('Spider', 'J', weight=1.0)
+
+G.add_edge('H', 'G', weight=1.0)
+G.add_edge('H', 'K', weight=1.0)
+
+G.add_edge('G', 'L', weight=1.0)
+G.add_edge('G', 'F', weight=1.0)
+
+G.add_edge('F', 'E', weight=1.0)
+
+G.add_edge('E', 'Fly', weight=1.0)
+
+G.add_edge('J', 'S', weight=1.0)
+G.add_edge('J', 'K', weight=1.0)
+
+G.add_edge('K', 'L', weight=1.0)
+G.add_edge('L', 'M', weight=1.0)
+G.add_edge('M', 'N', weight=1.0)
+G.add_edge('M', 'F', weight=1.0)
+
+G.add_edge('N', 'O', weight=1.0)
+G.add_edge('N', 'E', weight=1.0)
+
+G.add_edge('O', 'Fly', weight=1.0)
+
+G.add_edge('A', 'S', weight=1.0)
+G.add_edge('A', 'B', weight=1.0)
+
+G.add_edge('B', 'R', weight=1.0)
+G.add_edge('B', 'C', weight=1.0)
+
+G.add_edge('S', 'R', weight=1.0)
+G.add_edge('R', 'Q', weight=1.0)
+
+G.add_edge('Q', 'C', weight=1.0)
+G.add_edge('Q', 'P', weight=1.0)
+
+G.add_edge('C', 'D', weight=1.0)
+G.add_edge('D', 'Fly', weight=1.0)
+G.add_edge('P', 'D', weight=1.0)
+G.add_edge('P', 'O', weight=1.0)
+G.add_edge('O', 'Fly', weight=1.0)
+
+G.add_edge('T', 'Q', weight=1.0)
+G.add_edge('T', 'P', weight=1.0)
+G.add_edge('T', 'O', weight=1.0)
+G.add_edge('T', 'N', weight=1.0)
+G.add_edge('T', 'M', weight=1.0)
+
+G.add_edge('R', 'T', weight=1.0)
+G.add_edge('S', 'T', weight=1.0)
+G.add_edge('J', 'T', weight=1.0)
+G.add_edge('K', 'T', weight=1.0)
+G.add_edge('L', 'T', weight=1.0)
+
+nx.draw_networkx(G)
+GREEN = "#77DD77"
+BLUE = "#99CCFF"
+
+plt.plot(nx.draw(G, node_color=GREEN, with_labels=True))
+plt.show()
+#each edge has a weight of 1. The shortest path is the fewest edges.
+#Use this to verify that your graph built correctly.
+t = nx.shortest_path(G, 'Spider', 'Fly', weight='weight')
+
+print(t)
+
+
+
